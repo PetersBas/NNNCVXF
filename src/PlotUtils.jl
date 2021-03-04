9657f4a59c252aff802cb5ad311579c9d9bfb511
PetersBas <1.bas.peters@gmail.com>
2021-03-04 14:44:41 -0500

    updated plotting

--- src/PlotUtils.jl
+++ src/PlotUtils.jl
@@ -2,16 +2,31 @@ export PlotCamvidLossUnconstrained, PlotCamvidLossConstrained, PlotDataLabelPred
 
 function PlotCamvidLossUnconstrained(fval_train,fval_val,IoU_hist_train,IoU_hist_val)
 
+  f_log_train   = [];
+  f_log_val     = [];
+  # dc2_log_train = [];
+  # dc2_log_val   = [];
+  IoU_log_train = [[] []];
+  IoU_log_val   = [[] []];
+
+  for i=1:length(fval_train)
+    f_log_train     = vcat(f_log_train,fval_train[i])
+    f_log_val       = vcat(f_log_val,fval_val[i])
+    # dc2_log_train = vcat(dc2_log_train,fval_train[i])
+    # dc2_log_val   = vcat(dc2_log_val,fval_train[i])
+    IoU_log_train   = vcat(IoU_log_train,IoU_hist_train[i])
+    IoU_log_val     = vcat(IoU_log_val,IoU_hist_val[i])
+  end
 
   iter_ax = range(0,step=50,length=length(fval_train))
 
   figure(figsize=(16,7));
-  subplot(1,2,1);plot(iter_ax,fval_train,label="train");title("Training loss labels");
-  subplot(1,2,1);plot(iter_ax,fval_val,"--",label="validation");legend();title("Training loss labels");
-  subplot(1,2,2);plot(iter_ax,IoU_hist_train[:,1],label="class1 - train");title("IoU");
-  subplot(1,2,2);plot(iter_ax,IoU_hist_train[:,2],label="class2 - train");legend();title("IoU");
-  subplot(1,2,2);plot(iter_ax,IoU_hist_val[:,1],"--",label="class1 - validation");title("IoU");
-  subplot(1,2,2);plot(iter_ax,IoU_hist_val[:,2],"--",label="class2 - validation");legend();title("IoU");
+  subplot(1,2,1);plot(iter_ax,f_log_train,label="train");title("Training loss labels");
+  subplot(1,2,1);plot(iter_ax,f_log_val,"--",label="validation");legend();title("Training loss labels");
+  subplot(1,2,2);plot(iter_ax,IoU_log_train[:,1],label="class1 - train");title("IoU");
+  subplot(1,2,2);plot(iter_ax,IoU_log_train[:,2],label="class2 - train");legend();title("IoU");
+  subplot(1,2,2);plot(iter_ax,IoU_log_val[:,1],"--",label="class1 - validation");title("IoU");
+  subplot(1,2,2);plot(iter_ax,IoU_log_val[:,2],"--",label="class2 - validation");legend();title("IoU");
   xlabel("Iteration number");
   savefig("loss_unconstrained.png")
 
@@ -19,15 +34,31 @@ end
 
 function PlotCamvidLossConstrained(fval_train,fval_val,IoU_hist_train,IoU_hist_val,dc2val_train)
 
+  f_log_train   = [];
+  f_log_val     = [];
+  dc2_log_train = [];
+  #dc2_log_val   = [];
+  IoU_log_train = [[] []];
+  IoU_log_val   = [[] []];
+
+  for i=1:length(fval_train)
+    f_log_train     = vcat(f_log_train,fval_train[i])
+    f_log_val       = vcat(f_log_val,fval_val[i])
+    dc2_log_train   = vcat(dc2_log_train,dc2val_train[i])
+    #dc2_log_val   = vcat(dc2_log_val,fval_train[i])
+    IoU_log_train   = vcat(IoU_log_train,IoU_hist_train[i])
+    IoU_log_val     = vcat(IoU_log_val,IoU_hist_val[i])
+  end
+
   iter_ax = range(0,step=50,length=length(dc2val_train))
   figure(figsize=(16,7));
-  subplot(1,3,1);semilogy(iter_ax,dc2val_train,label="train");legend();title("Squared distance-to-set");
-  subplot(1,3,2);plot(iter_ax,fval_train,label="train");title("Training loss labels");
-  subplot(1,3,2);plot(iter_ax,fval_val,"--",label="validation");legend();title("Training loss labels");
-  subplot(1,3,3);plot(iter_ax,IoU_hist_train[:,1],label="class1 - train");title("IoU");
-  subplot(1,3,3);plot(iter_ax,IoU_hist_train[:,2],label="class2 - train");legend();title("IoU");
-  subplot(1,3,3);plot(iter_ax,IoU_hist_val[:,1],"--",label="class1 - validation");title("IoU");
-  subplot(1,3,3);plot(iter_ax,IoU_hist_val[:,2],"--",label="class2 - validation");legend();title("IoU");
+  subplot(1,3,1);semilogy(iter_ax,dc2_log_train,label="train");legend();title("Squared distance-to-set");
+  subplot(1,3,2);plot(iter_ax,f_log_train,label="train");title("Training loss labels");
+  subplot(1,3,2);plot(iter_ax,f_log_val,"--",label="validation");legend();title("Training loss labels");
+  subplot(1,3,3);plot(iter_ax,IoU_log_train[:,1],label="class1 - train");title("IoU");
+  subplot(1,3,3);plot(iter_ax,IoU_log_train[:,2],label="class2 - train");legend();title("IoU");
+  subplot(1,3,3);plot(iter_ax,IoU_log_val[:,1],"--",label="class1 - validation");title("IoU");
+  subplot(1,3,3);plot(iter_ax,IoU_log_val[:,2],"--",label="class2 - validation");legend();title("IoU");
   xlabel("Iteration number");
   savefig("loss_constrained.png")
 
