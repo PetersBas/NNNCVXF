b9d1d63715c9cf48a5d582ee5fef304b32f49d32
PetersBas <1.bas.peters@gmail.com>
2021-03-03 22:11:36 -0500

    added data plotting

--- src/PlotUtils.jl
+++ src/PlotUtils.jl
@@ -32,3 +32,42 @@ function PlotCamvidLossConstrained(fval_train,fval_val,IoU_hist_train,IoU_hist_v
   savefig("loss_constrained.png")
 
 end
+
+function PlotDataLabelPredictionCamvid(plt_ind::Int,data,label,HN,active_channels,tag::String)
+
+  #Plot data
+  figure(figsize=(5,4))
+  imshow(data[plt_ind][:,:,1:3,1])
+  title(string("Data - ",tag))
+  savefig(string(tag,"data.png"),bbox="tight")
+
+  #predict
+  p1, prediction, ~ = HN.forward(data[plt_ind], data[plt_ind])
+  prediction[:,:,active_channels,1].=softmax(prediction[:,:,active_channels,1],dims=3);
+
+  close("all")
+  vmi = 0.0
+  vma = 1.0
+  figure(figsize=(6,4))
+  subplot(2,2,1);imshow(Array(prediction)[3:end-2,3:end-2,1,1],vmin=vmi,vmax=vma);PyPlot.title("Prediction - class 1");colorbar()
+  subplot(2,2,2);imshow(Array(prediction)[3:end-2,3:end-2,2,1],vmin=vmi,vmax=vma);PyPlot.title("Prediction - class 2");colorbar()
+  subplot(2,2,3);imshow(label[plt_ind][3:end-2,3:end-2,1,1],vmin=vmi,vmax=vma);PyPlot.title("Label - class 1");colorbar()
+  subplot(2,2,4);imshow(label[plt_ind][3:end-2,3:end-2,2,1],vmin=vmi,vmax=vma);PyPlot.title("Label - class 2");colorbar()
+  savefig(string(tag,"_prediction_channels.png"))
+
+  #thresholded prediction
+  pred_thres = zeros(Int,size(prediction)[1:2])
+  pos_inds = findall(prediction[:,:,1,1] .> 0.65)#prediction[:,:,2,1])
+  pred_thres[pos_inds] .= 1
+  figure(figsize=(5,4));
+  imshow(Array(pred_thres)[3:end-2,3:end-2],vmin=vmi,vmax=vma);PyPlot.title(string("Prediction - ",tag));#xlabel("x");ylabel("y")
+  savefig(string(tag,"_prediction.png"))#,bbox="tight")
+
+  #Plot data+prediction
+  figure(figsize=(5,4))
+  imshow(data[plt_ind][3:end-2,3:end-2,1:3,1])
+  imshow(Array(pred_thres)[3:end-2,3:end-2],alpha=0.45)
+  title(string("Image+Prediction - ",tag))
+  savefig(string(tag,"data_plus_pred.png"))
+  return
+end
